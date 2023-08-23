e1c40825d14b2e3a8268ed87a7471926b9c6b46c
ZhannaZucher <jzucher@gmail.com>
2023-08-23 11:20:51 +0200

    add input placeholder & slice

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,32 +5,33 @@ import Article from "../components/Article"
 const Home = () => {
   const [data, setData] = useState([])
   const [query, setQuery] = useState("")
-  console.log(query)
 
   useEffect(() => {
     axios
-      .get("https://www.themealdb.com/api/json/v1/1/search.php?s")
+      .get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`)
       .then((res) => setData(res.data.meals))
-  }, [])
+  }, [query])
 
   return (
     <div className="home-wrapper">
       <form>
         <label htmlFor="search"></label>
-        <input type="search" onChange={(e) => setQuery(e.target.value)} />
+        <input
+          type="text"
+          placeholder="search an ingredient"
+          onChange={(e) => setQuery(e.target.value)}
+        />
       </form>
       <main className="content">
-        {data
-          .filter((recipe) => recipe.strMeal.toLowerCase().includes(query))
-          .map((recipe) => (
-            <Article
-              key={recipe.idMeal}
-              title={recipe.strMeal}
-              origin={recipe.strArea}
-              image={recipe.strMealThumb}
-              content={recipe.strInstructions}
-            />
-          ))}
+        {data?.slice(0, 24).map((recipe) => (
+          <Article
+            key={recipe.idMeal}
+            title={recipe.strMeal}
+            origin={recipe.strArea}
+            image={recipe.strMealThumb}
+            content={recipe.strInstructions}
+          />
+        ))}
       </main>
     </div>
   )
