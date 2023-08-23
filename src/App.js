ebfcc906ed7333fa984bad38a4c4685f22dcb4c7
ZhannaZucher <jzucher@gmail.com>
2023-08-23 18:34:09 +0200

    add routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-function App() {
-  return <div className="App">Hello world</div>
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Recipe from "./pages/Recipe"
+import Header from "./components/Header"
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/recipe/:id" element={<Recipe />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
