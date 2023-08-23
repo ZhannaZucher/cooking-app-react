56784538d68d6c5e7f0fe2b9c36aaa30f62b9891
ZhannaZucher <jzucher@gmail.com>
2023-08-23 09:42:41 +0200

    add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { NavLink } from "react-router-dom"
+
+const Header = () => {
+  return (
+    <header>
+      <NavLink to="/">Love Cooking</NavLink>
+    </header>
+  )
+}
+
+export default Header
